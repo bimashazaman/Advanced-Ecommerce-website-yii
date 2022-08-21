f18be9c7de0893d59a5a53ed8936fe4a8b3e9767
bimashazaman <bimashazaman1234@gmail.com>
2022-08-21 23:48:17 +0600

    new

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -12,9 +12,19 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+        'vendor/fontawesome-free/css/all.min.css',
+        'css/sb-admin-2.min.css',
+        'https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i'
     ];
     public $js = [
+        'vendor/jquery/jquery.min.js',
+        'vendor/bootstrap/js/bootstrap.bundle.min.js',
+        'vendor/jquery-easing/jquery.easing.min.js',
+        'js/sb-admin-2.min.js',
+        'vendor/chart.js/Chart.min.js',
+        'js/demo/chart-area-demo.js',
+        'js/demo/chart-pie-demo.js',
+        
     ];
     public $depends = [
         'yii\web\YiiAsset',
